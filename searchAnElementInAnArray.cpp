feeaef5cbcf94e0f1a511d32b67ed164f334dc07
sahil5131 <92348752+sahil5131@users.noreply.github.com>
2022-04-14 19:26:04 +0530

    Create searchAnElementInAnArray.cpp

---
+++ searchAnElementInAnArray.cpp
@@ -0,0 +1,12 @@
+int search(int arr[], int N, int X)
+{
+        
+    // Your code here
+    for(int i=0; i<N; i++){
+        if(arr[i]==X){
+            return i;
+        }
+    }
+    return -1;
+        
+}
