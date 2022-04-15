ba99487cb15eda1d77e8108ad131e6b04e52a8cc
sahil5131 <92348752+sahil5131@users.noreply.github.com>
2022-04-15 14:53:53 +0530

    Create twoDimensionalWorld.cpp

---
+++ twoDimensionalWorld.cpp
@@ -0,0 +1,14 @@
+void twoDimensional(/*Your Parameter here*/int **mat,int N){
+    
+    // Loop to iterate through matrix
+    for(int i = 0;i<N;i++){
+        for(int j = 0;j<N;j++){
+            
+            // Your code here
+            cout<<mat[i][j]<<" ";
+            
+        }
+        cout << endl;
+    }
+    
+}
