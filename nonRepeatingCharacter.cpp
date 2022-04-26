6d6b1a3ca4671d568f7387ecd961d12780fd403a
sahil5131 <92348752+sahil5131@users.noreply.github.com>
2022-04-26 18:05:38 +0530

    Create nonRepeatingCharacter.cpp

---
+++ nonRepeatingCharacter.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+    public:
+    //Function to find the first non-repeating character in a string.
+    char nonrepeatingCharacter(string S)
+    {
+       //Your code here
+       map<char, int> m;
+       for(int i=0; i<S.size(); i++){
+           m[S[i]]++;
+       }
+       for(int i=0; i<S.size(); i++){
+           if(m[S[i]]==1){
+               return S[i];
+           }
+       }
+       return '$';
+    }
+
+};
