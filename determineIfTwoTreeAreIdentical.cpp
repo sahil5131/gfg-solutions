cf0a7844f47fd97e3d13597dd855d289c8011f1e
sahil5131 <92348752+sahil5131@users.noreply.github.com>
2022-04-29 00:33:10 +0530

    Create determineIfTwoTreeAreIdentical.cpp

---
+++ determineIfTwoTreeAreIdentical.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+    public:
+    //Function to check if two trees are identical.
+    bool isIdentical(Node *r1, Node *r2)
+    {
+        //Your Code here
+        if(r1==NULL && r2==NULL){
+            return true;
+        }
+        else if(r1==NULL || r2==NULL){
+            return false;
+        }
+        if(r1->data==r2->data){
+            isIdentical(r1->left, r2->left);
+            isIdentical(r1->right, r2->right);
+        }
+        else return false;
+    }
+};
