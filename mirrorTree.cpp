4faa6e94b9323237e43626fc8972d434d2816f1b
sahil5131 <92348752+sahil5131@users.noreply.github.com>
2022-04-28 13:14:15 +0530

    Create mirrorTree.cpp

---
+++ mirrorTree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    // Function to convert a binary tree into its mirror tree.
+    
+    void mirror(Node* node) {
+        // code here
+        if(node==NULL){
+            return;
+        }
+        mirror(node->left);
+        mirror(node->right);
+        swap(node->left, node->right);
+    }
+};
